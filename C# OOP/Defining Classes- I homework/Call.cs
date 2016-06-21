7750d4747987ff0b4e3378d31aabf92fdd718f58
malamov <m_malamov@yahoo.com>
2016-06-21 20:54:36 +0300

    Add hw

---
+++ C# OOP/Defining Classes- I homework/Call.cs
@@ -0,0 +1,62 @@
+﻿namespace MobilePhone
+{
+    using System;
+
+   public class Call
+    {
+        private DateTime date;
+        private int dialedPhoneNumber;
+        private int duration;
+
+        public Call(DateTime date, int dialedPhoneNumber, int duration)
+        {
+            this.Date = date;
+            this.DialedPhoneNumber = dialedPhoneNumber;
+            this.Duration = duration;
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return this.date;
+            }
+            set
+            {
+                this.date = value;
+            }
+        }
+
+        public int DialedPhoneNumber
+        {
+            get
+            {
+                return this.dialedPhoneNumber;
+            }
+            set
+            {
+                this.dialedPhoneNumber = value;
+            }
+        }
+
+        public int Duration
+        {
+            get
+            {
+                return this.duration;
+            }
+            set
+            {
+                this.duration = value;
+            }
+
+        }
+
+        public override string ToString()
+        {
+            var callInfo = $"{this.Date}\t{this.Date}\t{this.dialedPhoneNumber}\t\t{this.Duration}";
+            return callInfo;
+        }
+
+    }
+}
---
+++ C# OOP/Defining Classes- I homework/GSMCallHistoryTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobilePhone
+{
+   public class GSMCallHistoryTest
+
+    {
+        private GSM Telephone = new GSM("Galaxy Tab", "Samsung", 720.99, "Slavcho", new Battery(BatteryType.LiIon, 400, 200), new Display(5.7, 16000000));
+
+        public void Test()
+        {
+            
+
+            Telephone.AddCall(new Call(new DateTime(2016, 06, 11), 0889312119, 350));
+            Telephone.AddCall(new Call(new DateTime(2016, 06, 12), 0887568431, 120));
+            Telephone.AddCall(new Call(new DateTime(2016, 06, 13), 0898658479, 280));
+            Console.WriteLine(Telephone.CallHistoryInfo());
+            double totalPrice = 0;
+            foreach (Call item in Telephone.CallHistory)
+            {
+                totalPrice += (double)(item.Duration / 60) * GSM.CallPrice;
+            }
+            Console.WriteLine("Total price of calls is: {0} BGN",totalPrice);
+            Console.WriteLine("Removing longest call.....");
+            double longestcall = 0;
+            foreach (Call item in Telephone.CallHistory)
+            {
+                longestcall = Math.Max(item.Duration, longestcall);
+                longestcall /= 60;
+            }
+            totalPrice = 0;
+            foreach (Call item in Telephone.CallHistory)
+            {
+                totalPrice += ((double)(item.Duration / 60) )* GSM.CallPrice;  
+            }
+            totalPrice -= longestcall * GSM.CallPrice;
+            Console.WriteLine("Total price of calls without longest call is is: {0:F2} BGN", totalPrice);
+            Telephone.ClearHistory();
+            Console.WriteLine(Telephone.CallHistoryInfo()); 
+
+
+
+        }
+
+        
+
+    }
+             
+    }
+
+
+   
